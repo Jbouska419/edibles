99b725321a38d6571c7b019858656b06da6ccb61
Jbouska419 <Jbouska419@gmail.com>
2013-08-08 00:45:35 -0500

    Create wedding.php

---
+++ wedding.php
@@ -0,0 +1,28 @@
+<?php # wedding.php
+$page_title = 'Weddings';
+include ('includes/header3.php');
+?>
+
+<div id = "content">
+  	<div class="cl20"></div>
+		<h1>weddings</h1>
+		<div class="cl20"></div>
+		<div id="gallery">
+		<a href="images/occasions/wedding/01.jpg" rel="gallery" title="mini wedding cakes"><img src="images/occasions/wedding/small/01_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/wedding/02.jpg" rel="gallery" title="wedding"><img src="images/occasions/wedding/small/02_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/wedding/03.jpg" rel="gallery" title="wedding"><img src="images/occasions/wedding/small/03_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/wedding/04.jpg" rel="gallery" title="beach theme wedding"><img src="images/occasions/wedding/small/04_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/wedding/05.jpg" rel="gallery" title="beach theme wedding closeup"><img src="images/occasions/wedding/small/05_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/wedding/06.jpg" rel="gallery" title="wedding closeup"><img src="images/occasions/wedding/small/06_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/wedding/07.jpg" rel="gallery" title="wedding closeup"><img src="images/occasions/wedding/small/07_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/wedding/08.jpg" rel="gallery" title="personalized wedding"><img src="images/occasions/wedding/small/08_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<div class="cl20"></div>
+			<h5>more photos coming soon...</h5>
+	</div>
+</div>
+
+
+<?php
+include ('includes/footer.html');
+?>
+
