67691ea636679a78bba386789a0ceb09b6010486
Jbouska419 <Jbouska419@gmail.com>
2013-08-08 00:36:22 -0500

    Create baptism.php

---
+++ baptism.php
@@ -0,0 +1,23 @@
+<?php # baptism.php
+$page_title = 'Baptisms';
+include ('includes/header3.php');
+?>
+
+<div id = "content">
+  	<div class="cl20"></div>
+		<h1>baptisms</h1>
+		<div class="cl20"></div>
+		<div id="gallery">
+		<a href="images/occasions/baptism/01.jpg" rel="gallery" title="baptism"><img src="images/occasions/baptism/small/01_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/baptism/02.jpg" rel="gallery" title="baptism"><img src="images/occasions/baptism/small/02_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/baptism/03.jpg" rel="gallery" title="baptism"><img src="images/occasions/baptism/small/03_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<div class="cl20"></div>
+			<h5>more photos coming soon...</h5>
+	</div>
+</div>
+
+
+<?php
+include ('includes/footer.html');
+?>
+
