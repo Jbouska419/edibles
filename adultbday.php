bfe6d9d8e377294b410e00c91f9795b040f18a40
Jbouska419 <Jbouska419@gmail.com>
2013-08-08 00:35:04 -0500

    Create adultbday.php

---
+++ adultbday.php
@@ -0,0 +1,35 @@
+<?php # adultbday.php
+$page_title = 'Adult Birthday';
+include ('includes/header3.php');
+?>
+
+<div id = "content">
+  	<div class="cl20"></div>
+		<h1>adult's birthday</h1>
+		<div class="cl20"></div>
+		<div id="gallery">
+		<a href="images/occasions/adult_birthday/01.jpg" rel="gallery" title="zebra theme"><img src="images/occasions/adult_birthday/small/01_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/adult_birthday/02.jpg" rel="gallery" title="golf theme for 65th birthday"><img src="images/occasions/adult_birthday/small/02_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/adult_birthday/03.jpg" rel="gallery" title="pharmacist/doctor theme"><img src="images/occasions/adult_birthday/small/03_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/adult_birthday/04.jpg" rel="gallery" title="Chicago sports"><img src="images/occasions/adult_birthday/small/04_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/adult_birthday/05.jpg" rel="gallery" title="sailing theme"><img src="images/occasions/adult_birthday/small/05_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/adult_birthday/06.jpg" rel="gallery" title="high end motorsports"><img src="images/occasions/adult_birthday/small/06_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/adult_birthday/07.jpg" rel="gallery" title="favorite things"><img src="images/occasions/adult_birthday/small/07_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/adult_birthday/08.jpg" rel="gallery" title="hair stylist theme"><img src="images/occasions/adult_birthday/small/08_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/adult_birthday/09.jpg" rel="gallery" title="drinks for 21st birthday"><img src="images/occasions/adult_birthday/small/09_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/adult_birthday/10.jpg" rel="gallery" title="Navy and White Sox"><img src="images/occasions/adult_birthday/small/10_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/adult_birthday/11.jpg" rel="gallery" title="pilot theme"><img src="images/occasions/adult_birthday/small/11_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/adult_birthday/12.jpg" rel="gallery" title="more favorite things"><img src="images/occasions/adult_birthday/small/12_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/adult_birthday/13.jpg" rel="gallery" title="manly theme"><img src="images/occasions/adult_birthday/small/13_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/adult_birthday/14.jpg" rel="gallery" title="more drinks for a 21st"><img src="images/occasions/adult_birthday/small/14_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/adult_birthday/15.jpg" rel="gallery" title="golf and penguins"><img src="images/occasions/adult_birthday/small/15_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<div class="cl20"></div>
+			<h5>more photos coming soon...</h5>
+	</div>
+</div>
+
+
+<?php
+include ('includes/footer.html');
+?>
+
