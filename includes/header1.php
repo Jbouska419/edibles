7b97cfa0c6495c7da7b5f9e6a7d74a6658a8b0ee
Jbouska419 <Jbouska419@gmail.com>
2013-08-08 00:28:40 -0500

    Create header1.php

---
+++ includes/header1.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>edibles by erin</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 0.20" />
+	<link rel = "stylesheet" type = "text/css" href = "style.css">
+	
+	<script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
+<script src="bxslider/jquery.bxSlider.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+  $(function(){
+  $('#js-slider').bxSlider({
+    auto: true,
+    autoControls : true,
+    prevText: null,
+    nextText: null,
+    stopText: null,
+    speed: 1500,
+  });
+}); 
+</script>
+	
+</head>
+
+<body>
+	<div id = "shell">
+		<div id = "header">
+		</div> <!-- header -->
+			<div class = "cl">
+			</div> <!-- cl -->
+				<!--<div id = "top-photo">
+				</div> <!-- top-photo -->
+				<div id="js-slider">
+					<div><img src="images/01.jpg" alt="salon"></div>
+				<!--<div><img src="images/photo02.jpg" alt="salon"></div> -->
+				<div><img src = "images/03.jpg" alt="restaurant"></div>
+				<!--<div><img src = "images/photo04.jpg" alt="vaccuum"></div> -->
+				</div> <!--js-slider-->
+					<div id="phone">
+					</div> <!--phone-->
+					<div class = "cl20">
+					</div> <!-- cl20 -->
+					
+					<div id = "nav">
+					<ul>
+						<li><a href = "index.php">Home</a></li>
+						<li><a href = "occasions.php">Occasions</a></li>
+						<li><a href = "flavors.php">Flavors</a></li>
+						<li><a href = "faq.php">F.A.Q.'s</a></li>
+						<li><a href = "pricing.php">Pricing</a></li>
+						<li><a href = "contact.php">Contact Us</a></li>
+					</ul>
+					</div> <!-- nav -->
+					<div id = "cl20">
+					</div> <!-- cl20 -->
+					
+					<div id="content">
+						<h1>Welcome to edibles by erin!</h1>
+						<div class="left-content">
+							<p>&nbsp;&nbsp;&nbsp;edibles by erin creates custom cake pops and more for your next event, or just to show someone
+							 how much you care.  Order now for birthdays, holidays, showers, weddings, bar/bat mitzvahs, 
+							 graduation, mother’s day and more! See our “occasions” page to begin your search for 
+							 something so unique, you’ll be the talk of the town!</p>
+							 
+							 <p>&nbsp;&nbsp;&nbsp;not sure what you want, but know you want ‘em?  No problem! We’re experts at creatively
+							  turning vague ideas into a beautiful centerpiece or favors!</p>
+							  
+							  <p>&nbsp;&nbsp;&nbsp;don’t be fooled by “cake pop makers” as seen on TV… our cake-pops are custom made, 
+							  “truffle-style”, hand shaped, hand dipped &amp; decorated.</p>
+							  
+							  <p>&nbsp;&nbsp;&nbsp;edibles by erin is fully licensed and insured to create treats to impress your guests at 
+							  your next event. need references? we’ve got ‘em!</p>
+							  
+							  <p>&nbsp;&nbsp;&nbsp;have fun with our website… and as always, please contact us if you have questions, to place 
+							  an order, or would like further information!</p>
+							  
+						</div> <!--left-content-->
+							  
+						<div class="right-content">
+							<div class="facebook">
+								<a href="http://www.facebook.com/EdiblesByErin"><img src="images/like.jpg" alt=""></a>
+							</div> <!--facebook-->
+							
+							<div class="ad">
+								<img src="images/ad.jpg" alt="">
+							</div> <!--ad-->
+							
+							<div class="form">
+							<form action="handle_form.php" method="post">
+							<fieldset>
+								<p><label><span>email:</span> <input type="text" name="email" size="10" maxlength="30" /></label></p>
+								<p><label><span>zip code:<span></span> <input type="text" name="zipcode" size="5" maxlength="5" /></label></p>
+							</fieldset>
+								<p align="center"><input type="submit" name="submit" value="Submit" /></p>	
+							</div> <!--form-->
+						</div> <!--right-content-->
+						<div class="cl">
+						</div> <!--cl-->
+					</div> <!-- content-->
