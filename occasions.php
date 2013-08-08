812fa1715d5b7a66549495e1dc77bab9a5473d58
Jbouska419 <Jbouska419@gmail.com>
2013-08-08 00:42:45 -0500

    Create occasions.php

---
+++ occasions.php
@@ -0,0 +1,100 @@
+<?php # occasions.php
+$page_title = 'occasions';
+include ('includes/header2.php');
+?>
+  	
+		<div id="content">
+			<div class="cl20"></div>
+			<h1>occasions</h1>
+			<div class="cl20"></div>
+			
+			<div class="occ-row-1">
+				<div class="box-1">
+					<a href="adultbday.php"><img src="images/occasions/adult_birthday/cover.jpg" alt="" title=""></a>
+					<span><a href="adultbday.php">Adult Birthday</a></span>
+				</div> <!--box-1-->
+				
+				<div class="box-2">
+					<a href="childbday.php"><img src="images/occasions/child_birthday/cover.jpg" alt="" title=""></a>
+					<span><a href="childbday.php">Child Birthday</a></span>
+				</div> <!--box-2-->
+				
+				<div class="box-3">
+					<a href="xmas.php"><img src="images/occasions/christmas/cover.jpg" alt="" title=""></a>
+					<span><a href="xmas.php">Christmas</a></span>
+				</div> <!--box-3-->
+				
+				<div class="box-4">
+					<a href="valentines.php"><img src="images/occasions/valentines/cover.jpg" alt="" title=""></a>
+					<span><a href="valentines.php">Valentine's</a></span>
+				</div> <!--box-4-->
+			
+			</div> <!--occ-row-1-->
+			
+			<div class="occ-row-2">
+				<div class="box-1">
+					<a href="graduation.php"><img src="images/occasions/graduation/cover.jpg" alt="" title=""></a>
+					<span><a href="graduation.php">Graduation</a></span>
+				</div> <!--box-1-->
+				
+				<div class="box-2">
+					<a href="momday.php"><img src="images/occasions/mothers_day/cover.jpg" alt="" title=""></a>
+					<span><a href="momday.php">Mother's Day</a></span>
+				</div> <!--box-2-->
+				
+				<div class="box-3">
+					<a href="teacher.php"><img src="images/occasions/teacher/cover.jpg" alt="" title=""></a>
+					<span class="long"><a href="teacher.php">Teacher Appreciation</a></span>
+				</div> <!--box-3-->
+				
+				<div class="box-4">
+					<a href="bridalshower.php"><img src="images/occasions/bridal_shower/cover.jpg" alt="" title=""></a>
+					<span><a href="bridalshower.php">Bridal Shower</a></span>
+				</div> <!--box-4-->
+			
+			</div> <!--occ-row-2 -->
+			
+			<div class="occ-row-3">
+				<div class="box-1">
+					<a href="babyshower.php"><img src="images/occasions/baby_shower/cover.jpg" alt="" title=""></a>
+					<span><a href="babyshower.php">Baby Shower</a></span>
+				</div> <!--box-1-->
+				
+				<div class="box-2">
+					<a href="wedding.php"><img src="images/occasions/wedding/cover.jpg" alt="" title=""></a>
+					<span><a href="wedding.php">Weddings</a></span>
+				</div> <!--box-2-->
+				
+				<div class="box-3">
+					<a href="baptism.php"><img src="images/occasions/baptism/cover.jpg" alt="" title=""></a>
+					<span><a href="baptism.php">Baptism</a></span>
+				</div> <!--box-3-->
+				
+				<div class="box-4">
+					<a href="justforfun.php"><img src="images/occasions/just_for_fun/cover.jpg" alt="" title=""></a>
+					<span><a href="justforfun.php">Just For Fun</a></span>
+				</div> <!--box-4-->
+			</div> <!--occ-row-3 -->
+			
+			<div class="cl40">
+			</div> <!--cl40-->
+			
+			<div class="center-text">
+				<p>each cake pop is meticulously hand crafted &amp; hand decorated to coordinate perfectly with your event.</p>
+				<p>We offer three different styles of cake creations:</p>
+				<h3>cake "pops"</h3>
+				<p>the cake truffle is above the stick. these work best for bouquet style displays and centerpieces.</p>
+				<h3>cake "bottoms"</h3>
+				<p>the stick sticks out atop the cake truffle and can stand alone. These work best for place-card favors, or to mix up in dessert table displays.</p>
+				<h3>"bitty bites"</h3>
+				<p>cake truffles without sticks. these work best when you don’t need a pop shaped, or intricately designed.</p>
+			</div><!--center-text-->
+			
+			<div class="cl100">
+			</div> <!-- cl100 -->
+			
+		</div> <!-- content -->
+		
+		<?php
+include ('includes/footer.html');
+?>
