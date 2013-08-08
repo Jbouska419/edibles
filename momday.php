5b9edac4b442aa611bb85daaa3369e7d7bd1ff10
Jbouska419 <Jbouska419@gmail.com>
2013-08-08 00:42:07 -0500

    Create momday.php

---
+++ momday.php
@@ -0,0 +1,24 @@
+<?php # momday.php
+$page_title = 'Graduation';
+include ('includes/header3.php');
+?>
+
+<div id = "content">
+  	<div class="cl20"></div>
+		<h1>mother's day</h1>
+		<div class="cl20"></div>
+		<div id="gallery">
+		<a href="images/occasions/mothers_day/01.jpg" rel="gallery" title="Mother's Day"><img src="images/occasions/mothers_day/small/01_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/mothers_day/02.jpg" rel="gallery" title="fruit theme"><img src="images/occasions/mothers_day/small/02_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/mothers_day/03.jpg" rel="gallery" title="garden theme"><img src="images/occasions/mothers_day/small/03_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/mothers_day/04.jpg" rel="gallery" title="garden closeup"><img src="images/occasions/mothers_day/small/04_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<div class="cl20"></div>
+			<h5>more photos coming soon...</h5>
+	</div>
+</div>
+
+
+<?php
+include ('includes/footer.html');
+?>
+
