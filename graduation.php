3f3bc0c37d5d757169a71914e684a6d5998de1f7
Jbouska419 <Jbouska419@gmail.com>
2013-08-08 00:40:21 -0500

    Create graduation.php

---
+++ graduation.php
@@ -0,0 +1,27 @@
+<?php # graduation.php
+$page_title = 'Graduation';
+include ('includes/header3.php');
+?>
+
+<div id = "content">
+  	<div class="cl20"></div>
+		<h1>graduation</h1>
+		<div class="cl20"></div>
+		<div id="gallery">
+		<a href="images/occasions/graduation/01.jpg" rel="gallery" title="Indiana University"><img src="images/occasions/graduation/small/01_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/graduation/02.jpg" rel="gallery" title="Indiana University closeup"><img src="images/occasions/graduation/small/02_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/graduation/03.jpg" rel="gallery" title="Mizzou"><img src="images/occasions/graduation/small/03_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/graduation/04.jpg" rel="gallery" title="Mizzou closeup"><img src="images/occasions/graduation/small/04_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/graduation/05.jpg" rel="gallery" title="doctor closeup"><img src="images/occasions/graduation/small/05_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/graduation/06.jpg" rel="gallery" title="general graduation"><img src="images/occasions/graduation/small/06_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/graduation/07.jpg" rel="gallery" title="U of I"><img src="images/occasions/graduation/small/07_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<div class="cl20"></div>
+			<h5>more photos coming soon...</h5>
+	</div>
+</div>
+
+
+<?php
+include ('includes/footer.html');
+?>
+
