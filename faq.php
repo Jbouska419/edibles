eeb54bf24aacb5395f05d5fd86d5f06155eac4a1
Jbouska419 <Jbouska419@gmail.com>
2013-08-08 00:38:55 -0500

    Create faq.php

---
+++ faq.php
@@ -0,0 +1,81 @@
+<?php # faq.php
+$page_title = 'F.A.Q.s';
+include ('includes/header2.php');
+?>
+
+  	<div id="content">
+		<div class="cl20"></div>
+			<h1>frequently asked questions</h1>
+		<div class="cl20"></div>
+			
+			<p class="question">I love your pictures! Do you have a store that I can come and see your pops?</p>
+			<p class="answer">At this time, we don’t have a storefront. Our pops are created in a licensed 
+			commercial kitchen and can be picked up or delivered.</p>
+			
+			<p class="question">delivery?! do you deliver?</p>
+			<p class="answer">We do deliver. See “pricing” for delivery fees.</p>
+			
+			<p class="question">what about shipping?</p>
+			<p class="answer">Please contact us about shipping. Some of our items can be shipped, many of the 
+			delicately detailed items can not. Additionally, due to the perishable nature of the pops, all shipping 
+			is required to be 1-day shipping. Please be advised that we do not offer ice packs or cooling devices 
+			and shipping is at the buyers risk. </p>
+			
+			<p class="question">i’ve seen those infomercials for “cake pop makers”… can’t I just buy one of those and 
+			make my own?</p>
+			<p class="answer">the cake pops created by edibles by erin are not made like the cakes that come out of 
+			the cake pop makers you see on TV.  The cake pops we make are baked cake that have been crumbled, blended 
+			with frosting, and shaped into the preferred shape. This results in a rich flavor with “truffle-style” 
+			consistency.</p>
+			
+			<p class="question">the place where I’m holding my event requires that outside treats come from a licensed 
+			and insured bakery…. are you?</p>
+			<p class="answer">yes! edibles by erin creates cake pops and goodies in a licensed commercial kitchen. We 
+			are fully insured and are a licensed business.</p>
+			
+			<p class="question">i love your cake pops, but I’m not sure what I want! can you help me!?</p>
+			<p class="answer">yes! Whether you have a vague idea or no idea at all, we can help create a beautiful, 
+			personalized arrangement for you.</p>
+			
+			<p class="question">your cakepops are amazing! do you make anything else!?</p>
+			<p class="answer">yes we do! We offer services to make full custom dessert tables, other goodies such as 
+			chocolate pretzel sticks, dipped crispy rice treats,  brownie pops,  dipped marshmallows, homemade marshmallows, 
+			homemade gumdrops and more! We also offer custom party table/decor services. Please contact us for more information</p>
+			
+			<p class="question">I have a wheat/nut/egg/dairy allergy. Can you create products that are guaranteed to be allergen free?</p>
+			<p class="answer">Unfortunately at this time, edibles by erin can NOT guarantee that any items are allergen free. 
+			Items are created in a facility and uses products that are exposed to all of the aforementioned allergens. 
+			Please inform anyone consuming items purchased from edibles by erin that they are not guaranteed allergen free.</p>
+			
+			<p class="question">But, can you do gluten free or sugar free items?</p>
+			<p class="answer">Please contact us for more information on this topic.</p>
+			
+			<p class="question">How far in advance do you need to place my order?</p>
+			<p class="answer">We prefer as much time as possible in case specialty decorations need to be ordered. During the 
+			holidays, or for large orders we need a minimum of 10 days prior to the event. Because we are a small business, 
+			often times holidays will sell-out so be sure to get your order in early for high volume times!</p>
+			
+			<p class="question">But… my mother-in-law’s birthday is next week! She REALLY wants cake pops, help!</p>
+			<p class="answer">call us, we will see what we can do.</p>
+			
+			<p class="question">How should I store them?</p>
+			<p class="answer">Your cake pops are obviously made of perishable materials. We recommend not refrigerating 
+			your cake pops until after you have presented them at your event. The reason for this is that (especially in 
+			the summer) the cake pops going from cold to hot will cause them to sweat and sometimes the moisture causes little 
+			spots on the cake pops. If you don’t mind little spots- refrigerate away! <br />
+			We definitely recommend storing in a cool, dry place. Your cake pops are made with candy coating on the outside, so 
+			they will melt in the heat.</p>
+			
+			<p class="question">Do you offer tastings?</p>
+			<p class="answer">Please contact us about tastings. For large events (where 100+ pops will be ordered) we can 
+			schedule a limited tasting after a deposit is secured.</p>
+			
+			<div class="cl100">
+			</div> <!--cl100-->
+			
+			</div> <!--content-->
+			
+			
+		<?php
+include ('includes/footer.html');
+?>
