7f5af4e500bd355cbc877f9c6f37443171589810
Jbouska419 <Jbouska419@gmail.com>
2013-08-08 00:37:36 -0500

    Create childbday.php

---
+++ childbday.php
@@ -0,0 +1,31 @@
+<?php # childbday.php
+$page_title = 'Child Birthday';
+include ('includes/header3.php');
+?>
+
+<div id = "content">
+  	<div class="cl20"></div>
+		<h1>child's birthday</h1>
+		<div class="cl20"></div>
+		<div id="gallery">
+		<a href="images/occasions/child_birthday/01.jpg" rel="gallery" title="Phineas & Ferb"><img src="images/occasions/child_birthday/small/01_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/child_birthday/02.jpg" rel="gallery" title="Mickey Mouse"><img src="images/occasions/child_birthday/small/02_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/child_birthday/03.jpg" rel="gallery" title="more Mickey"><img src="images/occasions/child_birthday/small/03_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/child_birthday/04.jpg" rel="gallery" title="dinosaurs"><img src="images/occasions/child_birthday/small/04_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/child_birthday/05.jpg" rel="gallery" title="dino closeup"><img src="images/occasions/child_birthday/small/05_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/child_birthday/06.jpg" rel="gallery" title="Star Wars"><img src="images/occasions/child_birthday/small/06_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/child_birthday/07.jpg" rel="gallery" title="Batman"><img src="images/occasions/child_birthday/small/07_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/child_birthday/08.jpg" rel="gallery" title="monkeys and reindeer"><img src="images/occasions/child_birthday/small/08_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/child_birthday/09.jpg" rel="gallery" title="Handy Manny"><img src="images/occasions/child_birthday/small/09_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/child_birthday/10.jpg" rel="gallery" title="catepillar"><img src="images/occasions/child_birthday/small/10_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/child_birthday/11.jpg" rel="gallery" title="Avengers"><img src="images/occasions/child_birthday/small/11_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<div class="cl20"></div>
+			<h5>more photos coming soon...</h5>
+	</div>
+</div>
+
+
+<?php
+include ('includes/footer.html');
+?>
+
