8cbe5da9c0f7a11ce1e5752c35408d61d4aa1bc3
Jbouska419 <Jbouska419@gmail.com>
2013-08-08 00:39:40 -0500

    Create flavors.php

---
+++ flavors.php
@@ -0,0 +1,58 @@
+<?php # flavors.php
+$page_title = 'flavors';
+include ('includes/header2.php');
+?>
+
+  
+		<div id="content">
+			<div class="cl20"></div>
+			<h1>traditional</h1>
+			<div class="cl20"></div>
+			<ul> 
+				<li><span>milk-chocolate</span> - rich, chocolatey cake with milk-chocolate frosting</li>
+				<li><span>dark chocolate</span> - deep, dark chocolate with dark chocolate frosting 
+						(try dark chocolate pops with  raspberry, orange or mint)</li>
+				<li><span>yellow</span> - traditional buttery yellow cake with creamy vanilla frosting</li>
+				<li><span>vanilla cake</span> - vanilla cake with fresh vanilla bean frosting</li>
+				<li><span>red velvet</span> - rich, deep red cake mixed with your choice of cream cheese or vanilla frosting</li>
+				<li><span>lemon</span> - bright, refreshing lemon flavored cake  with subtle lemon frosting</li>
+				<li><span>pineapple</span> - pineapple cake</li>
+				<li><span>strawberry</span> - strawberry flavored cake mixed with vanilla frosting</li>
+				<li><span>spice</span> - spicy blend of nutmeg, cinnamon, &amp; spices blended with cream cheese or vanilla frosting</li>
+				<li><span>chocolate peanut butter</span> - chocolatey cake with creamy peanut butter </li>
+			</ul>
+			<div class="cl20"></div>
+			<h1>premium</h1>
+			<div class="cl20"></div>
+			<ul> 
+				<li><span>cinnamon roll</span> - rich, buttery cinnamon cake with cream cheese frosting</li>
+				<li><span>fresh banana bread cake</span> - fresh banana bread blended with vanilla or cream cheese frosting (with or without walnuts)</li>
+				<li><span>irish cream &amp; chocolate</span> - rich chocolate cake blended with irish cream liquor</li>
+				<li><span>cookies &amp; cream</span> - crushed chocolate cream cookies blended with vanilla frosting</li>
+				<li><span>chocolate chip cookie dough (made without eggs)</span> - just like mom used to make but with tiny chips</li>
+				<li><span>s’mores</span> - chocolate cake blended with grahams, topped with a mini marshmallow  (not able to be shaped, spheres only)</li>
+				<li><span>pb&amp;j</span> - peanut butter cake blended with fresh strawberry frosting</li>
+				<li><span>chocolate covered cherry cake</span> - cherry cake blended with chocolate frosting</li>
+				<li><span>swirly</span> - two or more colors of cake side by side, so when the pop is bitten, it looks like a swirl of colors (fun for kids parties)</li>
+			</ul>
+			<div class="cl20"></div>
+			<h1>seasonal</h1>
+			<div class="cl20"></div>
+			<ul> 
+				<li><span>pumpkin spice (oct.-dec.)</span> - a rich blend of  fall spices and creamy pumpkin cake with cream cheese frosting</li>
+				<li><span>chocolate peppermint (nov.-jan.)</span> - decadent chocolate with a burst of peppermint in every bite</li>
+				<li><span>egg nog (nov.-jan.)</span> - a seasonal favorite! A buttery cake with a light bourbon frosting. (can be made without bourbon)</li>
+				<li><span>fresh strawberry (april-sept.)</span> - light vanilla cake with bursts of fresh strawberries</li>
+				<li><span>fresh peach (may-sept.)</span> - juicy peaches blended into vanilla cake with vanilla frosting</li>
+				<li><span>fresh berry (may-aug.)</span> - vanilla cake swirled with blueberries &amp; blackberries with vanilla frosting</li>
+				<li><span>fresh apple cinnamon (aug.-nov.)</span> - seasonal apples baked into vanilla cake with cinnamon frosting</li>
+			</ul>
+			
+			<div class="cl100">
+		</div><!--cl100-->
+		
+		</div> <!--content-->
+		
+		<?php
+include ('includes/footer.html');
+?>
