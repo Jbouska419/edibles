d9e1bfb29848545f8d7c93db75f8b42c686f2d48
Jbouska419 <Jbouska419@gmail.com>
2013-08-08 00:43:18 -0500

    Create pricing.php

---
+++ pricing.php
@@ -0,0 +1,81 @@
+<?php # pricing.php
+$page_title = 'pricing';
+include ('includes/header2.php');
+?>
+
+  	<div id="content">
+			<div class="cl20"></div>
+			<h1>pricing</h1>
+			<div class="cl20"></div>
+			
+			<div class="center-text">
+			<p>due to the unique nature of each order, pricing will vary from what is listed below. <br />
+			prices are based on the intricacy of the designs. <br />
+			minimum order is one dozen and increases in ½ dozen increments. <br />
+			cake flavors are one flavor per dozen pops. <br />
+			prices below are for “traditional flavors” only (see flavor section).</p>
+			
+			<div class="cl20">
+			</div> <!--cl20-->
+			
+			<h2>cake pops and “bottom” pops</h2> 
+			<p>sphere pops (plain pops with simple design or sprinkles) begin at $22/dozen <br />
+			shaped pops (anything other then spheres) begin at $30/dozen <br />
+			design pops (more than one color dip, hand placed décor, etc) begin at $30/dozen</p>
+			
+			<div class="cl20">
+			</div> <!--cl20-->
+			
+			<h2>bitty bites (cake truffles without a stick, spheres only)</h2>
+			<p>begin at  $15/dozen</p>
+			
+			<div class="cl20">
+			</div> <!--cl20-->
+			
+			<h2>premium &amp; seasonal flavors</h2>
+			<p>add $3 per dozen</p>
+			
+			<div class="cl20">
+			</div> <!--cl20-->
+			
+			<h2>display / individually wrapping</h2>
+			<p>prices for displays begin at $5 <br />
+			prices for individually wrapping begin at $5</p>
+			
+			<div class="cl20">
+			</div> <!--cl20-->
+			
+			<h2>delivery</h2>
+			<p>delivery times are on a first come, first served basis<br />
+			there is no guarantee that delivery will be available until booked <br />
+			within 10 miles of 60540- $10 <br />
+			$1 per mile outside that area <br />
+			$40 to deliver to the city of Chicago, plus parking fee if we need to park</p>
+			
+			<div class="cl20">
+			</div> <!--cl20-->
+			
+			<h2>set up fees</h2>
+			<p>$15 per hour set up fee (minimum one hour) <br />
+			this includes set up for the dessert table, or favor table, <br />
+			and setting out cake pops with labels/display
+			</p>
+			
+			<div class="cl20">
+			</div> <!--cl20-->
+			
+			<h2>vase/display rental</h2>
+			<p>please contact us for this pricing information.</p>
+			
+			</div> <!--center-text-->
+			
+			<div class="cl100">
+			</div> <!--cl100-->
+			
+		</div> <!--content-->
+		
+		
+		<?php
+include ('includes/footer.html');
+?>
+			
