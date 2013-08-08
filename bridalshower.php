80c0824d2dd009f4516a1a53e0e7a3d5e5d96f9b
Jbouska419 <Jbouska419@gmail.com>
2013-08-08 00:37:05 -0500

    Create bridalshower.php

---
+++ bridalshower.php
@@ -0,0 +1,22 @@
+<?php # bridalshower.php
+$page_title = 'Bridal Shower';
+include ('includes/header3.php');
+?>
+
+<div id = "content">
+  	<div class="cl20"></div>
+		<h1>bridal showers</h1>
+		<div class="cl20"></div>
+		<div id="gallery">
+		<a href="images/occasions/bridal_shower/01.jpg" rel="gallery" title="beach theme"><img src="images/occasions/bridal_shower/small/01_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/bridal_shower/02.jpg" rel="gallery" title="beach theme closeup"><img src="images/occasions/bridal_shower/small/02_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<div class="cl20"></div>
+			<h5>more photos coming soon...</h5>
+	</div>
+</div>
+
+
+<?php
+include ('includes/footer.html');
+?>
+
