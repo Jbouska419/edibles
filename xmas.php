ae085a4d6cd296338495b97f4b2bdd3fc4b2ee42
Jbouska419 <Jbouska419@gmail.com>
2013-08-08 00:46:07 -0500

    Create xmas.php

---
+++ xmas.php
@@ -0,0 +1,27 @@
+<?php # xmas.php
+$page_title = 'Christmas';
+include ('includes/header3.php');
+?>
+
+<div id = "content">
+  	<div class="cl20"></div>
+		<h1>christmas</h1>
+		<div class="cl20"></div>
+		<div id="gallery">
+		<a href="images/occasions/christmas/01.jpg" rel="gallery" title="Christmas"><img src="images/occasions/christmas/small/01_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/christmas/02.jpg" rel="gallery" title="Christmas mug"><img src="images/occasions/christmas/small/02_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/christmas/03.jpg" rel="gallery" title="simple Christmas mug"><img src="images/occasions/christmas/small/03_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/christmas/04.jpg" rel="gallery" title="winter wonderland"><img src="images/occasions/christmas/small/04_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/christmas/05.jpg" rel="gallery" title="winter wonderland closeup"><img src="images/occasions/christmas/small/05_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/christmas/06.jpg" rel="gallery" title="Christmas"><img src="images/occasions/christmas/small/06_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/christmas/07.jpg" rel="gallery" title="melting snowman cookie"><img src="images/occasions/christmas/small/07_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<div class="cl20"></div>
+			<h5>more photos coming soon...</h5>
+	</div>
+</div>
+
+
+<?php
+include ('includes/footer.html');
+?>
+
