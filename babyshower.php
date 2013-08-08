6b401bc2145baa4131cbb00c807d21cd66153a4c
Jbouska419 <Jbouska419@gmail.com>
2013-08-08 00:35:46 -0500

    Create babyshower.php

---
+++ babyshower.php
@@ -0,0 +1,26 @@
+<?php # babyshower.php
+$page_title = 'Baby Shower';
+include ('includes/header3.php');
+?>
+
+<div id = "content">
+  	<div class="cl20"></div>
+		<h1>baby showers</h1>
+		<div class="cl20"></div>
+		<div id="gallery">
+		<a href="images/occasions/baby_shower/01.jpg" rel="gallery" title="rubber duckies"><img src="images/occasions/baby_shower/small/01_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/baby_shower/02.jpg" rel="gallery" title="ducky mug"><img src="images/occasions/baby_shower/small/02_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/baby_shower/03.jpg" rel="gallery" title="baby shower"><img src="images/occasions/baby_shower/small/03_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/baby_shower/04.jpg" rel="gallery" title="baby shower"><img src="images/occasions/baby_shower/small/04_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/baby_shower/05.jpg" rel="gallery" title="baby shower"><img src="images/occasions/baby_shower/small/05_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/baby_shower/06.jpg" rel="gallery" title="baby shower closeup"><img src="images/occasions/baby_shower/small/06_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<div class="cl20"></div>
+			<h5>more photos coming soon...</h5>
+	</div>
+</div>
+
+
+<?php
+include ('includes/footer.html');
+?>
+
