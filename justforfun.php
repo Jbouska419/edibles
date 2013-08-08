ba0f2c2ec369233668ec7f6f49b570e2cec5f87e
Jbouska419 <Jbouska419@gmail.com>
2013-08-08 00:41:39 -0500

    Create justforfun.php

---
+++ justforfun.php
@@ -0,0 +1,30 @@
+<?php # justforfun.php
+$page_title = 'Just For Fun';
+include ('includes/header3.php');
+?>
+
+<div id = "content">
+  	<div class="cl20"></div>
+		<h1>just for fun</h1>
+		<div class="cl20"></div>
+		<div id="gallery">
+		<a href="images/occasions/just_for_fun/01.jpg" rel="gallery" title="casino theme"><img src="images/occasions/just_for_fun/small/01_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/just_for_fun/02.jpg" rel="gallery" title="casino closeup"><img src="images/occasions/just_for_fun/small/02_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/just_for_fun/03.jpg" rel="gallery" title="casino closeup"><img src="images/occasions/just_for_fun/small/03_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/just_for_fun/04.jpg" rel="gallery" title="autumn theme"><img src="images/occasions/just_for_fun/small/04_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/just_for_fun/05.jpg" rel="gallery" title="autumn theme"><img src="images/occasions/just_for_fun/small/05_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/just_for_fun/06.jpg" rel="gallery" title="just for fun"><img src="images/occasions/just_for_fun/small/06_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/just_for_fun/07.jpg" rel="gallery" title="just for fun"><img src="images/occasions/just_for_fun/small/07_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/just_for_fun/08.jpg" rel="gallery" title="just for fun"><img src="images/occasions/just_for_fun/small/08_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/just_for_fun/09.jpg" rel="gallery" title="just for fun"><img src="images/occasions/just_for_fun/small/09_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/just_for_fun/10.jpg" rel="gallery" title="mini smores"><img src="images/occasions/just_for_fun/small/10_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<div class="cl20"></div>
+			<h5>more photos coming soon...</h5>
+	</div>
+</div>
+
+
+<?php
+include ('includes/footer.html');
+?>
+
