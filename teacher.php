9bb8095d2f3b211d2e9beb435db37fec1ed1e7a0
Jbouska419 <Jbouska419@gmail.com>
2013-08-08 00:44:27 -0500

    Create teacher.php

---
+++ teacher.php
@@ -0,0 +1,24 @@
+<?php # teacher.php
+$page_title = 'Teacher Appreciation';
+include ('includes/header3.php');
+?>
+
+<div id = "content">
+  	<div class="cl20"></div>
+		<h1>teacher appreciation</h1>
+		<div class="cl20"></div>
+		<div id="gallery">
+		<a href="images/occasions/teacher/01.jpg" rel="gallery" title="teacher appreciation"><img src="images/occasions/teacher/small/01_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/teacher/02.jpg" rel="gallery" title="teacher appreciation"><img src="images/occasions/teacher/small/02_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/teacher/03.jpg" rel="gallery" title="teacher appreciation"><img src="images/occasions/teacher/small/03_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/teacher/04.jpg" rel="gallery" title="teacher appreciation"><img src="images/occasions/teacher/small/04_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<div class="cl20"></div>
+			<h5>more photos coming soon...</h5>
+	</div>
+</div>
+
+
+<?php
+include ('includes/footer.html');
+?>
+
