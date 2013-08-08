29be6ccd8de4945e67604faeac153e4b5fb295be
Jbouska419 <Jbouska419@gmail.com>
2013-08-08 00:45:00 -0500

    Create valentines.php

---
+++ valentines.php
@@ -0,0 +1,25 @@
+<?php # valentines.php
+$page_title = 'Valentine\'s Day';
+include ('includes/header3.php');
+?>
+
+<div id = "content">
+  	<div class="cl20"></div>
+		<h1>valentine's day</h1>
+		<div class="cl20"></div>
+		<div id="gallery">
+		<a href="images/occasions/valentines/01.jpg" rel="gallery" title="Valentines"><img src="images/occasions/valentines/small/01_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/valentines/02.jpg" rel="gallery" title="Valentines"><img src="images/occasions/valentines/small/02_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/valentines/03.jpg" rel="gallery" title="Valentines"><img src="images/occasions/valentines/small/03_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/valentines/04.jpg" rel="gallery" title="Valentines"><img src="images/occasions/valentines/small/04_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<a href="images/occasions/valentines/05.jpg" rel="gallery" title="Valentines"><img src="images/occasions/valentines/small/05_a.jpg" width="133" height="100" alt="adultbday"></a>
+		<div class="cl20"></div>
+			<h5>more photos coming soon...</h5>
+	</div>
+</div>
+
+
+<?php
+include ('includes/footer.html');
+?>
+
