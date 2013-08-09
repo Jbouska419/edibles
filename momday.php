b78a548e3c1b165371889091fca2d8ad1f4ef46c
Jbouska419 <Jbouska419@gmail.com>
2013-08-09 16:07:36 -0500

    Update momday.php
    added previous and next arrows to page

--- momday.php
+++ momday.php
@@ -4,9 +4,15 @@ include ('includes/header3.php');
 ?>
 
 <div id = "content">
-  	<div class="cl20"></div>
+		<div class="cl20"></div>
 		<h1>mother's day</h1>
 		<div class="cl20"></div>
+		<div id="previous-arrow">
+				<a href="graduation.php"><img src="images/previous-arrow.png" alt="previous arrow"></a>
+			</div>
+			<div id="next-arrow">
+				<a href="teacher.php"><img src="images/next-arrow.png" alt="next arrow"></a>
+			</div>
 		<div id="gallery">
 		<a href="images/occasions/mothers_day/01.jpg" rel="gallery" title="Mother's Day"><img src="images/occasions/mothers_day/small/01_a.jpg" width="133" height="100" alt="adultbday"></a>
 		<a href="images/occasions/mothers_day/02.jpg" rel="gallery" title="fruit theme"><img src="images/occasions/mothers_day/small/02_a.jpg" width="133" height="100" alt="adultbday"></a>
@@ -15,6 +21,7 @@ include ('includes/header3.php');
 		<div class="cl20"></div>
 			<h5>more photos coming soon...</h5>
 	</div>
+	<div class="cl40"></div>
 </div>
 
 
