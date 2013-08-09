0c6b26b5b79e25168d25bf9ef4fa42b9bd9c3b25
Jbouska419 <Jbouska419@gmail.com>
2013-08-09 16:08:22 -0500

    Update teacher.php
    added previous and next arrows to page

--- teacher.php
+++ teacher.php
@@ -4,9 +4,15 @@ include ('includes/header3.php');
 ?>
 
 <div id = "content">
-  	<div class="cl20"></div>
+		<div class="cl20"></div>
 		<h1>teacher appreciation</h1>
 		<div class="cl20"></div>
+		<div id="previous-arrow">
+				<a href="momday.php"><img src="images/previous-arrow.png" alt="previous arrow"></a>
+			</div>
+			<div id="next-arrow">
+				<a href="bridalshower.php"><img src="images/next-arrow.png" alt="next arrow"></a>
+			</div>
 		<div id="gallery">
 		<a href="images/occasions/teacher/01.jpg" rel="gallery" title="teacher appreciation"><img src="images/occasions/teacher/small/01_a.jpg" width="133" height="100" alt="adultbday"></a>
 		<a href="images/occasions/teacher/02.jpg" rel="gallery" title="teacher appreciation"><img src="images/occasions/teacher/small/02_a.jpg" width="133" height="100" alt="adultbday"></a>
@@ -15,6 +21,7 @@ include ('includes/header3.php');
 		<div class="cl20"></div>
 			<h5>more photos coming soon...</h5>
 	</div>
+	<div class="cl40"></div>
 </div>
 
 
