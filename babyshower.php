c63920ae819c1068df2e4e41317f7f6ed6f5e212
Jbouska419 <Jbouska419@gmail.com>
2013-08-09 16:02:27 -0500

    Update babyshower.php
    added previous and next arrows to page

--- babyshower.php
+++ babyshower.php
@@ -4,9 +4,15 @@ include ('includes/header3.php');
 ?>
 
 <div id = "content">
-  	<div class="cl20"></div>
+		<div class="cl20"></div>
 		<h1>baby showers</h1>
 		<div class="cl20"></div>
+		<div id="previous-arrow">
+				<a href="bridalshower.php"><img src="images/previous-arrow.png" alt="previous arrow"></a>
+			</div>
+			<div id="next-arrow">
+				<a href="wedding.php"><img src="images/next-arrow.png" alt="next arrow"></a>
+			</div>
 		<div id="gallery">
 		<a href="images/occasions/baby_shower/01.jpg" rel="gallery" title="rubber duckies"><img src="images/occasions/baby_shower/small/01_a.jpg" width="133" height="100" alt="adultbday"></a>
 		<a href="images/occasions/baby_shower/02.jpg" rel="gallery" title="ducky mug"><img src="images/occasions/baby_shower/small/02_a.jpg" width="133" height="100" alt="adultbday"></a>
@@ -17,6 +23,7 @@ include ('includes/header3.php');
 		<div class="cl20"></div>
 			<h5>more photos coming soon...</h5>
 	</div>
+	<div class="cl40"></div>
 </div>
 
 
